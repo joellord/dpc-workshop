1fc942fdc77a2f8735b6aac4f7d8b5c130cf2b50
Joel Lord <joelphy@gmail.com>
2019-06-04 16:35:10 -0500

    Fixed issue with small weather component

--- part7/start/components/Weather/Small.js
+++ part7/start/components/Weather/Small.js
@@ -1,17 +1,6 @@
 import React, {Component} from "react";
 
 export default class Small extends Component {
-  constructor(props) {
-    super(props);
-    if (!props.dt) {
-      props = {
-        dt: 0,
-        temp: {max: 0, min: 0}
-      };
-    }
-    this.state = props;
-  }
-
   render() {
     let dayOfWeek = (dt) => {
       let d = new Date(dt * 1000);
@@ -20,8 +9,8 @@ export default class Small extends Component {
 
     return(
       <div className="col center border">
-        Weather for {dayOfWeek(this.state.dt)} <br/>
-        {this.state.temp.max} C / {this.state.temp.min} C
+        Weather for {dayOfWeek(this.props.dt)} <br/>
+        {this.props.temp.max} C / {this.props.temp.min} C
       </div>
     )
   }
